4b1932b1cbf6c2217cc3bb8e1a9209e5bb648120
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-03 15:27:33 -0800

    timestamp

--- models/recipe.js
+++ models/recipe.js
@@ -6,9 +6,10 @@ const Recipe = new Schema(
     name: { type: String, required: true },
     description: { type: String, required: true },
     image: { type: String, required: true }, 
-    done: {type: Boolean, default: false}
+    done: {type: Boolean, default: false},
+    date: {type: Number, default: Math.floor(Date.now() / 1000) }
   }, 
-  {timestamps:{ currentTime: () => Math.floor(Date.now() / 1000) }}
+ {timestamps:true}
 )
 
 module.exports = mongoose.model('recipes',Recipe)
