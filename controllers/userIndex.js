8c2f2e82eb0af78c2881bbb6fc86240b66190739
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-05 16:47:08 -0800

    sign up

--- controllers/userIndex.js
+++ controllers/userIndex.js
@@ -1,34 +1,45 @@
 const User = require('../models/user')
 const Bcrypt = require('bcryptjs');
 
-const createUser = async (req,res) => {
-    // async await only for returning PROMISES 
-    const user = new User(req.body)
-    await user.save() 
-    return res.status(201).json(user)
-  }
-  
-  // these have the req, res functions so dont need to put in app.get(). 
-  
-  const getAllUsers = async (req, res) => {
-    const users = await User.find()
-    return res.status(200).json(users)
-  }
-  
-  const getUserById = async (req, res) => {
-    const { id } = req.params
-    const user = await User.findById(id)
-    return res.status(200).json(user)
-  }
-  
-  const updateUser = async (req, res) => {
-    const { id } = req.params 
-    await User.findByIdAndUpdate(id, req.body, { new: true }, (err, user) => {
-      if (err) return res.status(500).send(err)
+const createUser = async (request,response) => {
+    // async await only for returning PROMISES
+    try { 
+      // check if user already exists
+      var user = await User.findOne({ username: request.body.username }).exec();
       if (!user) {
-        return res.status(500).send('User not found')
+    const newUser = new User(request.body)
+    await newUser.save() 
+    return response.status(201).json(newUser)
       }
-      return res.status(200).json(user)
+      else {
+        return response.status(400).json({ message: "username already exists" });
+      }
+    }
+    catch (e) {
+      response.status(500).send(e);
+    }
+  }
+  // these have the request, response functions so dont need to put in app.get(). 
+  
+  const getAllUsers = async (request, response) => {
+    const users = await User.find()
+    return response.status(200).json(users)
+  }
+  
+  const getUserById = async (request, response) => {
+    const { id } = request.params
+    const user = await User.findById(id)
+    return response.status(200).json(user)
+  }
+  
+  const updateUser = async (request, response) => {
+    const { id } = request.params 
+    await User.findByIdAndUpdate(id, request.body, { new: true }, (err, user) => {
+      if (err) return response.status(500).send(err)
+      if (!user) {
+        return response.status(500).send('User not found')
+      }
+      return response.status(200).json(user)
     })
   
   }
@@ -46,14 +57,14 @@ const createUser = async (req,res) => {
         response.status(500).send(error);
     }
 };
-  const deleteUser = async (req, res) => {
-    const { id } = req.params 
+  const deleteUser = async (request, response) => {
+    const { id } = request.params 
     try {
       const user = await User.findByIdAndDelete(id)
-      res.status(200).json(user + ' deleted')
+      response.status(200).json(user + ' deleted')
     }
     catch (e) {
-      return res.send(id + ' not found')
+      return response.send(id + ' not found')
     }
     
   }
